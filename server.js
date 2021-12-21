85b6e82a63b7e104df454b53df4bcb71f8008265
KAYITARE-C <ani7kayitare@gmail.com>
2021-12-21 11:22:58 +0200

    change the port

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ mongoose.connect(dbUrl,{
     useUnifiedTopology:true,
     // useFindAndModify:false,
 }).then(()=>console.log("Database connected successfully"))
-
-app.listen(3030, () => {
-  console.log(`server is running on port 3030`);
+const port=process.env.PORT;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
 });
 
 export default app;
